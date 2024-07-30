9a2d8d3b79da7fdf931033d5c5fbf5d85a6e38a9
Amy Ye <amyye@Amys-MacBook-Air.local>
2024-07-30 10:45:11 +1000

    Initial commit

--- src/User.java
+++ src/User.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+
 public class User {
     private String username;
     private String password;
 
+
+    private ArrayList<ToDoItem> toDoItems;
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.toDoItems = new ArrayList<>();
     }
 
     /**
@@ -36,5 +42,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public ArrayList<ToDoItem> getToDoItems() {
+        return toDoItems;
+    }
+
+
+
 }
 
